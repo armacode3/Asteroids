5d58bb8e0601d8d2c29aeff90dc117469458e9db
armacode3 <zavaarmando0@gmail.com>
2025-09-30 16:10:06 -0400

    Player can now move using wasd

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import pygame
+from constants import *
+from player import Player
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("black")
+        player1.update(dt)
+        player1.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+    pygame.quit()
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
